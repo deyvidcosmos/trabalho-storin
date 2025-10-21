242d5c249a7774c5d5cf2ade6886026e076e553e
João Pedro das Neves <jp.neves195@gmail.com>
2025-10-21 11:00:51 -0300

    EmpregadoHorista.java

--- EmpregadoHorista.java
+++ EmpregadoHorista.java
@@ -1,1 +1,35 @@
-sfdfdf
+class EmpregadoHorista extends Object {
+    private String nome;
+    private double horasTrabalhadas;
+    private double valorHora;
+    
+    public EmpregadoHorista (String nome, double horasTrabalhadas, double valorHora){
+        this.nome = nome;
+        this.horasTrabalhadas = horasTrabalhadas;
+        this.valorHora = valorHora;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public double getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+    public void setHorasTrabalhadas(double horasTrabalhadas){
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+    public double getValorHoras() {
+        return valorHora;
+    }
+    public double ganhos () {
+        return horasTrabalhadas * valorHora;
+    }
+    @Override
+    public String toString () {
+        return String.format ("EmpregadoHorista %s %.2f %.2f", nome, horasTrabalhadas, valorHora);
+    }
+
+}
